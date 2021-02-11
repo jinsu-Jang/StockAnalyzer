580c962826ebe19daae784a556e354ffc40dfd70
jinsu-jang <dajoa72@naver.com>
2021-02-11 22:31:28 +0900

    Add node.js Express

---
+++ stockclient/src/components/User.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+class User extends React.Component {
+    render() {
+        return (
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
+        )
+    }
+}
+
+class UserProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.image} alt="profile"/>
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class UserInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
+        )
+    }
+}
+    
+export default User;
+
