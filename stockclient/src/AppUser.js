0dacb360c82342952dcc18d03d70fced35c42728
jinsu-Jang <dajoa72@naver.com>
2021-03-07 13:02:13 +0900

    Add React

---
+++ stockclient/src/AppUser.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import User from './components/User'
+import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { withStyles } from '@material-ui/core/styles';
+import UserAdd from './components/UserAdd'
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2),
+
+  }
+});
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { 
+        userList : [],
+        usrs: '',
+        completed: 0
+    };
+  } 
+
+  // state = {
+  //   usrs: '',
+  //   completed: 0
+  // }
+
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+    this.callApi()
+      .then(res => this.setState({users: res}))
+      .catch(err => console.log(err));
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  callApi = async () => {
+    let api_url = "http://127.0.0.1:5000/users";
+    let options = [];
+    fetch(api_url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({userList:data});
+        this.setState({users:data.user_list});
+      });
+    // const body = await response.json();
+    // return body;
+
+    // const response = await fetch("http://127.0.0.1:5000/users");
+    // const body = await response.json();
+    // return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {/* {console.log(this.state.users)} */}
+              {this.state.users ?
+                this.state.users.map(c => {
+                  return <User key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+                }) :
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                  </TableCell>
+                </TableRow>
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+
+export default withStyles(styles)(App);
+
---
+++ stockclient/src/routes/PrivateRoutes.js
@@ -0,0 +1,33 @@
+import React, { Suspense, lazy } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import SLUGS from 'resources/slugs';
+import LoadingComponent from 'components/loading';
+import {Reports}  from 'pages/Reports';
+
+const DashboardComponent = lazy(() => import('./dashboard'));
+
+function PrivateRoutes() {
+    return (
+        <Suspense fallback={<LoadingComponent loading />}>
+            <Switch>
+                <Route exact path={SLUGS.dashboard} component={DashboardComponent} />
+                <Route exact path={SLUGS.overviewTwo} render={() => <Reports />} />
+                <Route exact path={SLUGS.overviewThree} render={() => <div>overviewThree</div>} />
+                <Route exact path={SLUGS.overviewFour} render={() => <div>overviewFour</div>} />
+                <Route exact path={SLUGS.overview} render={() => <div>overview</div>} />
+                <Route exact path={SLUGS.reports} render={() => <div>reprots</div>} />
+                <Route exact path={SLUGS.ideasTwo} render={() => <div>ideasTwo</div>} />
+                <Route exact path={SLUGS.ideasThree} render={() => <div>ideasThree</div>} />
+                <Route exact path={SLUGS.ideas} render={() => <div>ideas</div>} />
+                <Route exact path={SLUGS.contacts} render={() => <div>contacts</div>} />
+                <Route exact path={SLUGS.agents} render={() => <div>agents</div>} />
+                <Route exact path={SLUGS.articles} render={() => <div>articles</div>} />
+                <Route exact path={SLUGS.settings} render={() => <div>settings</div>} />
+                <Route exact path={SLUGS.subscription} render={() => <div>subscription</div>} />
+                <Redirect to={SLUGS.dashboard} />
+            </Switch>
+        </Suspense>
+    );
+}
+
+export default PrivateRoutes;
